8fd423d1a3d20f2ac80d6e7ea7b0f328612ced3e
sathish <sathishkumar1131@gmail.com>
2024-12-22 00:33:42 +0530

    initial setup

---
+++ src/main/java/com/project/testdata/TestdataApplication.java
@@ -0,0 +1,13 @@
+package com.project.testdata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestdataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestdataApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/testdata/TestdataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.testdata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestdataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
